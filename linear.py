d252b061b459bb334ee2e2ad548c457344c2b305
shehamane <shehamane@mail.ru>
2022-04-11 18:04:58 +0300

    SAGD не работает пока..(

--- linear.py
+++ linear.py
@@ -2,7 +2,8 @@ import random
 from abc import ABC, abstractmethod
 import numpy as np
 from data import add_ones_feature
-from quality_functional import LossFunction, MSE
+from quality_functional import LossFunction, MSE, MAE
+from copy import deepcopy
 
 
 class Model(ABC):
@@ -30,7 +31,7 @@ class AnalyticalSolution(Model):
         return design_matrix.dot(self.w)
 
 
-class GradientDescend(Model, ABC):
+class GradientDescent(Model, ABC):
     def __init__(self):
         self.w = None
         self.reg_lmb = None
@@ -47,15 +48,15 @@ class GradientDescend(Model, ABC):
             self.w -= (self.reg_lmb / N) * np.sign(self.w)
 
 
-class StandardGradientDescend(GradientDescend):
+class StandardGradientDescent(GradientDescent):
     def __init__(self, alpha, S: int, tolerance: float = 1, regularization=None, reg_lmb=None,
-                 descend_method: str = 'const', loss: LossFunction = MSE):
+                 descent_method: str = 'const', loss: LossFunction = MSE):
         self.w = None
-        self.descend_method = descend_method
-        if descend_method == 'const' or descend_method == 'normalization_const':
+        self.Descent_method = descent_method
+        if descent_method == 'const' or descent_method == 'normalization_const':
             self.alpha = alpha
         if S <= 0 or tolerance <= 0:
-            raise Exception('S or tolerance must be positive')
+            raise Exception('S and tolerance must be positive')
         self.S = S
         self.tolerance = tolerance
         self.regularization = regularization
@@ -64,7 +65,7 @@ class StandardGradientDescend(GradientDescend):
 
     def get_gradient(self, X: np.ndarray, y: np.ndarray):
         gradient = (2 * X.T.dot(X.dot(self.w) - y)) / X.shape[0]
-        if self.descend_method == 'normalization_const':
+        if self.Descent_method == 'normalization_const':
             gradient /= np.linalg.norm(gradient)
 
         return gradient
@@ -86,49 +87,79 @@ class StandardGradientDescend(GradientDescend):
         return design_matrix.dot(self.w)
 
 
-class StohasticAverageGradient:
-    def __init__(self, alpha, forgetting_rate, E=1, tolerance=1):
-        self.alpha = alpha
-        self.forgetting_rate = forgetting_rate
-        self.E = max(1, E)
-        self.moving_average = 0
-        self.tolerance = tolerance
+# class StochasticAverageGradientDescent(GradientDescent):
+#     def __init__(self, alpha, E: int, tolerance: float = 1, forgetting_rate: float = 0.5, regularization=None,
+#                  reg_lmb=None,
+#                  descent_method: str = 'const', loss: LossFunction = MSE):
+#         self.grads = None
+#         self.descent_method = descent_method
+#         self.alpha = alpha
+#         self.grads_sum = None
+#         self.forgetting_rate = forgetting_rate
+#         self.moving_average = 0
+#         self.w = None
+#         self.Descent_method = descent_method
+#         if descent_method == 'const' or descent_method == 'normalization_const':
+#             self.alpha = alpha
+#         if E <= 0 or tolerance <= 0:
+#             raise Exception('E and tolerance must be positive')
+#         self.E = E
+#         self.tolerance = tolerance
+#         self.regularization = regularization
+#         self.reg_lmb = reg_lmb
+#         self.loss = loss
+#
+#     def get_gradient(self, X, y):
+#         gradient = (2 * X.T.dot(X.dot(self.w) - y)) / X.shape[0]
+#
+#         return gradient
+#
+#     def calc_grads(self, X, y):
+#         self.grads = np.empty(X.shape)
+#         for i in range(0, X.shape[0]):
+#             self.grads[i] = self.get_gradient(X[i], y[i])
+#             self.grads_sum += self.grads[i]
+#
+#     def calc_moving_average(self, new_error: float):
+#         if self.moving_average > 0:
+#             self.moving_average *= 1 - self.forgetting_rate
+#         self.moving_average += new_error * self.forgetting_rate
+#
+#     def recalc_grad_sum(self, old_grad, new_grad):
+#         self.grads_sum -= old_grad - new_grad
+#
+#     def recalc_grad(self, X, y, idx):
+#         old_grad = deepcopy(self.grads[idx])
+#         self.grads[idx] = self.get_gradient(X, y)
+#         self.recalc_grad_sum(old_grad, self.grads[idx])
+#
+#     def shuffle(self, X, y) -> (np.ndarray, np.ndarray):
+#         M = np.c_[X, y]
+#         np.random.shuffle(M)
+#         return M[:, :-1], M[:, -1]
+#
+#     def fit(self, X: np.ndarray, y: np.ndarray):
+#         X = add_ones_feature(X)
+#
+#         self.w = np.zeros(shape=X.shape[1])
+#         self.grads_sum = np.zeros(shape=X.shape[1])
+#         self.calc_grads(X, y)
+#
+#         for e in range(0, self.E):
+#             X, y = self.shuffle(X, y)
+#             for i in range(0, X.shape[0]):
+#                 self.recalc_grad(X[i], y[i], i)
+#
+#                 if self.descent_method == 'normalization const':
+#                     self.w -= (self.alpha / X.shape[0]) * self.grads_sum / np.linalg.norm(self.grads_sum)
+#                 else:
+#                     self.w -= (self.alpha / X.shape[0]) * self.grads_sum
+#
+#                 err = MSE.get_single_loss(y[i], X[i].dot(self.w))
+#                 self.calc_moving_average(err)
+#
+#     def predict(self, design_matrix: np.ndarray):
+#         design_matrix = add_ones_feature(design_matrix)
+#         return design_matrix.dot(self.w)
 
-    def calc_moving_average(self, new_error: float):
-        if self.moving_average > 0:
-            self.moving_average *= 1 - self.forgetting_rate
-        self.moving_average += new_error * self.forgetting_rate
 
-    def recalc_grad_sum(self, old_grad, new_grad):
-        self.grads_sum += new_grad - old_grad
-
-    def recalc_grad(self, X, y, idx):
-        if not self.is_calculated[idx]:
-            self.grads[idx] = (2 * X.T.dot(X.dot(self.w) - y)) / X.shape[0]
-            self.recalc_grad_sum(0, self.grads[idx])
-            self.is_calculated[idx] = 1
-        else:
-            old_grad = self.grads[idx]
-            self.grads[idx] = (2 * X.T.dot(X.dot(self.w) - y)) / X.shape[0]
-            self.recalc_grad_sum(old_grad, self.grads[idx])
-
-    def fit(self, design_matrix: np.ndarray, target: np.ndarray):
-        design_matrix = add_ones_feature(design_matrix)  # Добавляет признак-единичку
-
-        self.grads = -np.ones(shape=design_matrix.shape)
-        self.grads_sum = np.zeros(shape=design_matrix.shape[1])
-        self.is_calculated = np.zeros(shape=design_matrix.shape[0])
-        self.w = np.zeros(shape=design_matrix.shape[1])
-
-        for e in range(0, self.E):
-            for _ in range(0, design_matrix.shape[0]):
-                i = random.randint(0, design_matrix.shape[0] - 1)
-                self.recalc_grad(design_matrix[i], target[i], i)  # Пересчитать градиент
-                self.w -= self.alpha * self.grads_sum / design_matrix.shape[0]  # Обновить веса
-
-                err = MSE.get_single_loss(target[i], design_matrix[i].dot(self.w))
-                self.calc_moving_average(err)  # Пересчитать скользящую среднюю
-
-    def predict(self, design_matrix: np.ndarray):
-        design_matrix = add_ones_feature(design_matrix)
-        return design_matrix.dot(self.w)
