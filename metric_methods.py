ec4c8eb02458c8f6b06e892aee998d9aaecfaf3f
shehamane <shehamane@mail.ru>
2022-04-30 18:36:17 +0300

    weighted kNN, new distance metrics

--- metric_methods.py
+++ metric_methods.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 import pandas as pd
@@ -5,17 +7,23 @@ import metrics
 
 
 class kNearestNeighbors:
-    __metrics_map = {'euclidian': metrics.EuclidianNorm}
+    __metrics_map = {'euclidian': metrics.EuclidianNorm,
+                     'manhattan': metrics.ManhattanNorm,
+                     'cosine': metrics.CosineDistance}
 
     def __init__(self, k, X: pd.DataFrame, y: pd.Series, metric: str = 'euclidian',
-                 search_method='exhaustive', choice_method='maxentry'):
+                 search_method='exhaustive', choice_method='maxentry', window_width=None):
         self.__k = k
         self.__search_method = search_method
         self.__metrics = self.__class__.__metrics_map[metric]
         self.__choice_method = choice_method
+        self.__window_width = window_width
+        self.__classes = y.unique()
         if search_method == 'exhaustive':
             self.__data = X.to_numpy()
             self.__target = y.to_numpy()
+        if choice_method == 'weighted' and window_width is None:
+            raise Exception('Window width is not defined')
 
     def set_metrics(self, metric):
         self.__metrics = self.__class__.metrics_map[metric]
@@ -41,18 +49,37 @@ class kNearestNeighbors:
             idx += 1
         return neighbors_idx
 
-    def __get_most_freq_target(self, objects_idx: list):
-        targets = self.__target[objects_idx]
-        unique, pos = np.unique(targets, return_inverse=True)
+    def __get_most_freq_class(self, objects_idx: list):
+        target = self.__target[objects_idx]
+        unique, pos = np.unique(target, return_inverse=True)
         maxpos = np.bincount(pos).argmax()
         return unique[maxpos]
+
+    def __get_most_heavy_class(self, u: np.ndarray, objects_idx: list):
+        def gaussian_kernel(x):
+            return math.pow(math.e, -2 * math.pow(x, 2)) / math.sqrt(2 * math.pi)
+
+        max_sum = 0
+        max_class = None
+        for cls in self.__classes:
+            sum = 0
+            for idx in objects_idx:
+                if self.__target[idx] == cls:
+                    sum += gaussian_kernel(self.__metrics.get_distance(u, self.__data[idx]) / self.__window_width)
+            if sum > max_sum:
+                max_sum = sum
+                max_class = cls
+
+        return max_class
 
     def __predict_once(self, x: np.ndarray):
         if self.__search_method == 'exhaustive':
             neighbors_idx = self.__exhaustive_search(x)
 
         if self.__choice_method == 'maxentry':
-            return self.__get_most_freq_target(neighbors_idx)
+            return self.__get_most_freq_class(neighbors_idx)
+        elif self.__choice_method == 'weighted':
+            return self.__get_most_heavy_class(x, neighbors_idx)
 
     def predict(self, X: pd.DataFrame):
         predictions = np.empty(shape=X.shape[0], dtype='O')
--- metrics.py
+++ metrics.py
@@ -14,3 +14,15 @@ class EuclidianNorm(Norm):
     @staticmethod
     def get_distance(x: np.ndarray, y: np.ndarray):
         return np.sqrt(np.sum(np.power(x - y, 2)))
+
+
+class ManhattanNorm(Norm):
+    @staticmethod
+    def get_distance(x: np.ndarray, y: np.ndarray):
+        return np.sum(np.abs(x - y))
+
+
+class CosineDistance(Norm):
+    @staticmethod
+    def get_distance(x: np.ndarray, y: np.ndarray):
+        return 1 - (x.dot(y))/(np.linalg.norm(x)*np.linalg.norm(y))
