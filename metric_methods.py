0194c30c988625e91ef6d9627d675f9c7c78341f
shehamane <shehamane@mail.ru>
2022-05-10 01:03:33 +0300

    removed exhaustive search, kdtree added

--- metric_methods.py
+++ metric_methods.py
@@ -1,6 +1,7 @@
 import math
 
 import numpy as np
+from scipy.spatial import KDTree
 
 import pandas as pd
 import metrics
@@ -12,8 +13,7 @@ class kNearestNeighbors:
                      'cosine': metrics.CosineDistance}
 
     def __init__(self, k, X: pd.DataFrame, y: pd.Series, metric: str = 'euclidian',
-                 search_method='exhaustive', choice_method='maxentry', window_width=None):
-
+                 search_method='kdtree', choice_method='popular'):
         if k > 0:
             self.__k = k
         else:
@@ -26,23 +26,27 @@ class kNearestNeighbors:
 
         self.__metrics = self.__class__.__metrics_map[metric]
 
-        if choice_method in ['maxentry', 'weighted', 'kernel-regression']:
+        if choice_method in ['popular', 'weighted', 'kernel-regression']:
             self.__choice_method = choice_method
         else:
             raise Exception(f'No such choice method: {choice_method}')
-
-        if window_width is not None and window_width > 0 or choice_method == 'maxentry':
-            self.__window_width = window_width
-        else:
-            raise Exception('Window width must be positive')
 
         if X.shape[0] != y.shape[0]:
             raise Exception('X and y are incompatible shapes')
         if choice_method != 'kernel-regression':
             self.__classes = y.unique()
-        if search_method == 'exhaustive':
-            self.__data = X.to_numpy()
-            self.__target = y.to_numpy()
+        if search_method == 'kdtree':
+            if metric == 'cosine':
+                raise Exception('Cosine metric and k-d tree are incompatible')
+            self.__kdtree = KDTree(X.to_numpy())
+
+        if metric == 'euclidian':
+            self.__norm = 2
+        elif metric == 'manhattan':
+            self.__norm = 1
+
+        self.__X = X.to_numpy()
+        self.__y = y.to_numpy()
 
     def set_metrics(self, metric):
         self.__metrics = self.__class__.__metrics_map[metric]
@@ -50,51 +54,26 @@ class kNearestNeighbors:
     def set_choice_method(self, choice_method):
         self.__choice_method = choice_method
 
-    def __exhaustive_search(self, x: np.ndarray) -> (np.ndarray, np.ndarray):
-        neighbors_idx = []
-        neighbors_dists = []
-        idx = 0
-        for obj in self.__data:
-            dist = self.__metrics.get_distance(x, obj)
-            if len(neighbors_idx) >= self.__k:
-                for i in range(self.__k):
-                    if dist < neighbors_dists[i]:
-                        neighbors_dists[i] = dist
-                        neighbors_idx[i] = idx
-                        break
-            else:
-                neighbors_dists.append(dist)
-                neighbors_idx.append(idx)
-            idx += 1
-        return self.__data[neighbors_idx], self.__target[neighbors_idx]
-
-    def __search(self, x: np.ndarray) -> (np.ndarray, np.ndarray):
-        if self.__search_method == 'exhaustive':
-            return self.__exhaustive_search(x)
-
     @staticmethod
     def __gaussian_kernel(x):
         return math.pow(math.e, -2 * math.pow(x, 2)) / math.sqrt(2 * math.pi)
 
-    def __nadarai_watson(self, x, neighbors: np.ndarray, targets: np.ndarray):
+    def __nadarai_watson(self, targets, distances, window_width):
         return np.sum(
-            [self.__gaussian_kernel(
-                self.__metrics.get_distance(x, neighbors[i]) / self.__window_width
-            ) * targets[i] for i in range(0, neighbors.shape[0])]
-        ) \
-               / np.sum(
-            [self.__gaussian_kernel(
-                self.__metrics.get_distance(x, neighbor) / self.__window_width
-            ) for neighbor in neighbors]
+            [self.__gaussian_kernel(distances[i] / window_width) * targets[i]
+             for i in range(0, self.__k)]
+        ) / np.sum(
+            [self.__gaussian_kernel(distances[i] / window_width)
+             for i in range(0, self.__k)]
         )
 
     @staticmethod
-    def __get_most_freq_class(targets: np.ndarray):
+    def __get_most_popular_class(targets: np.ndarray):
         unique, pos = np.unique(targets, return_inverse=True)
         maxpos = np.bincount(pos).argmax()
         return unique[maxpos]
 
-    def __get_most_heavy_class(self, x, neighbors: np.ndarray, targets: np.ndarray):
+    def __get_most_heavy_class(self, targets: np.ndarray, distances, window_width):
 
         max_sum = 0
         max_class = None
@@ -102,32 +81,33 @@ class kNearestNeighbors:
             weighted_sum = 0
             for i, target in enumerate(targets):
                 if target == cls:
-                    weighted_sum += self.__gaussian_kernel(self.__metrics.get_distance(x, neighbors[i])
-                                                           / self.__window_width)
+                    weighted_sum += self.__gaussian_kernel(distances[i] / max(window_width, 10-6))
             if weighted_sum > max_sum:
                 max_sum = weighted_sum
                 max_class = cls
 
         return max_class
 
-    def __predict_once(self, x: np.ndarray):
-        neighbors, targets = None, None
-        if self.__search_method == 'exhaustive':
-            neighbors, targets = self.__search(x)
-
-        if self.__choice_method == 'maxentry':
-            return self.__get_most_freq_class(targets)
+    def __choice_class(self, tt, dd):
+        predicts = None
+        if self.__choice_method == 'popular':
+            predicts = [self.__get_most_popular_class(t) for t in tt]
         elif self.__choice_method == 'weighted':
-            return self.__get_most_heavy_class(x, neighbors, targets)
+            predicts = [self.__get_most_heavy_class(tt[i][:-1], dd[i][:-1], dd[i][-1])
+                        for i in range(0, tt.shape[0])]
         elif self.__choice_method == 'kernel-regression':
-            return self.__nadarai_watson(x, neighbors, targets)
+            predicts = [self.__nadarai_watson(tt[i][:-1], dd[i][:-1], dd[i][-1])
+                        for i in range(0, tt.shape[0])]
+        return predicts
 
     def predict(self, X: pd.DataFrame):
-        if self.__choice_method == 'kernel-regression':
-            predictions = np.empty(shape=X.shape[0])
-        else:
-            predictions = np.empty(shape=X.shape[0], dtype='O')
         X = X.to_numpy()
-        for i, obj in enumerate(X):
-            predictions[i] = self.__predict_once(obj)
-        return predictions
+
+        predicts = None
+        if self.__search_method == 'kdtree':
+            dd, ii = self.__kdtree.query(X, k=self.__k + int(self.__choice_method != 'popular'),
+                                         p=self.__norm)
+            tt = np.array([self.__y[i] for i in ii])
+            predicts = self.__choice_class(tt, dd)
+
+        return np.array(predicts)
